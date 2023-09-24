3fe767d73cf47574d33dd19095327e6d3485771e
- <=>
2023-09-25 04:33:11 +0530

    icon done

--- src/routes/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import Login from '../pages/Authentication/Login'
-// import {  Navigate } from 'react-router-dom';
-
-// const PrivateRoute = ({ element, isAuth }) => {
-//   return isAuth ? element : <Navigate to="/login" />
-// };
-
-// export default PrivateRoute;
